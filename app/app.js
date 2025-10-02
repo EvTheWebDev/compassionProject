16b911f9c15e49b3f3ed6db4e2c4ba7a7124df53
Ev Schmitt <evanjschmitt@gmail.com>
2025-10-01 22:27:59 -0400

    Site Update #2: Routing Changes & Styling Updates

--- app/app.js
+++ app/app.js
@@ -1,36 +1,61 @@
 function changeRoute() {
-let hashTag = window.location.hash;
- let pageID = hashTag.replace('#', '');
-//   console.log(hashTag + ' ' + pageID);
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  //   console.log(hashTag + ' ' + pageID);
 
-if (pageID != '') {
-$.get(`pages/${pageID}.html`, function (data) {
- console.log('data ' + data);
- $('#app').html(data);
-});
-} else {
-$.get(`pages/home.html`, function (data) {
-console.log('data ' + data);
- $('#app').html(data);
-});
-}
-// Bottom of changeRoute
+  if (pageID != "") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      console.log("data " + data);
+      $("#app").html(data);
+    });
+  } else {
+    $.get(`pages/home.html`, function (data) {
+      // console.log('data ' + data);
+      $("#app").html(data);
+    });
+  }
+  // Bottom of changeRoute
 }
 
 function initURLListener() {
-$(window).on('hashchange', changeRoute);
-changeRoute();
-// Bottom of initURLListener
+  $(window).on("hashchange", changeRoute);
+  changeRoute();
+  // Bottom of initURLListener
 }
 
 function initListeners() {
+  let hamburgerIcon = document.getElementById("hamburgerIcon");
+  let rippleEffect = document.getElementById("mobileOverlay");
+  let mobileNav = document.getElementById("mobileNav");
 
-    // Bottom of initListeners
+  hamburgerIcon.addEventListener("click", function () {
+    if (mobileNav.style.display === "block") {
+      mobileNav.style.display = "none";
+      rippleEffect.style.display = "none";
+    } else {
+      mobileNav.style.display = "block";
+      rippleEffect.style.display = "block";
+    }
+  });
+
+  rippleEffect.addEventListener("click", function () {
+    mobileNav.style.display = "none";
+    rippleEffect.style.display = "none";
+  });
+
+// Hide mobileNav and rippleEffect when any mobileNav link is clicked
+mobileNav.querySelectorAll('a').forEach(function(link) {
+    link.addEventListener('click', function () {
+        mobileNav.style.display = "none";
+        rippleEffect.style.display = "none";
+    });
+});
+
+  // Bottom of initListeners
 }
 
 $(document).ready(function () {
-initURLListener();
-initListeners();
-// Bottom of document.ready
+  initURLListener();
+  initListeners();
+  // Bottom of document.ready
 });
-
